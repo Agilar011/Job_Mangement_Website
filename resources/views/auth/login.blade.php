ea9995c8e75769bf30720c50778cce8955f21ccb
adimas_surya <105150571+adimas90@users.noreply.github.com>
2023-09-23 20:21:00 +0700

    Login, Register, landing Clear

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
         <x-validation-errors class="mb-4" />
 
         @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+            <div class="mb-4 font-medium text-sm text-green-600">
                 {{ session('status') }}
             </div>
         @endif
@@ -28,21 +28,23 @@
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
-                <x-button class="ml-4">
+                <x-button class="ml-4" style="background-color: #1FA809 ;">
                     {{ __('Log in') }}
                 </x-button>
             </div>
+
+            <p style="text-align: center; margin-top:50px; font-size:14px;">Belum Punya akun? <span style="color:rgb(30, 44, 197) ;"><a href="{{ route('register') }}">Ajukan Pendaftaran</a></span></p>
         </form>
     </x-authentication-card>
 </x-guest-layout>
